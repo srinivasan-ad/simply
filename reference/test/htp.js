cc73f603e8ca16294acf8b1444f12a672a8ae184
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-04-10 22:01:08 +0530

    :sparkles:

--- reference/test/htp.js
+++ reference/test/htp.js
@@ -1,5 +1,5 @@
 const http = require('http')
-
+//creating a basicserver 
 http.createServer((req,res)=>{res.write('JAI SRI RAM');
 res.end()}
 ).listen(5000,()=>console.log("Server is running ....."))
